98a136632751cde2a839e6fef50aeebaf2fa8635
Kushagra <kushagra100chhabra@gmail.com>
2025-07-13 00:15:17 +0530

    DF

--- vite-project/src/assets/Components2/United_Kingdom2.jsx
+++ vite-project/src/assets/Components2/United_Kingdom2.jsx
@@ -7,30 +7,36 @@ import Home2 from './Home2';
 export default function United_Kingdom2() {
     const united = [
         {
-            name: "(i) Imber Village",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPvdraViS_xtLQBz0uk5xiVxb3dEoxX5uAFg&s",
-            para: "Imber is a deserted village in Wiltshire, evacuated in 1943 for D-Day training. The villagers were never allowed to return."
+            name: "Champness Hall",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSptLvxumoVMKx0xszvh5ZAyd2MX-rEv-g-3Q&s",
+            para: `Champness Hall in Rochdale is believed to be haunted. Many reports describe unexplained noises, shadowy figures, and cold spots within the building. Some stories even detail ghostly encounters with a figure resembling a woman named Mary from the Sisters of Mercy, who were associated with the hall in the past`
         },
         {
-            name: "(ii) Capel Celyn",
-            link: "https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/CE65/production/_128573825_try5gettyimages-592205406.jpg",
-            para: "Capel Celyn, a Welsh-speaking village in Wales, was controversially flooded in 1965 to supply water to Liverpool."
+            name: "Chillingham Castle ",
+            link: "https://i.ytimg.com/vi/EMkXU-XZs8Y/maxresdefault.jpg",
+            para: `Chillingham Castle is widely regarded as one of the most haunted castles in England and is known for its high levels of paranormal activity. The castle boasts a long and often brutal history, with numerous reported sightings of ghosts and other supernatural phenomena. `
         },
         {
-            name: "(iii) Aldwych Underground Station",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHD9wkd0W0rAr_UOaxABx4sENdxl4VoR7P3A&s",
-            para: "Aldwych Station in London closed in 1994. Known for its elegant architecture, it’s now used for filming and tours."
+            name: " Tower of London",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqcISTLThhkc_nI13Uju9CgIBZKC0IcJqL4fM8jzZsumrUI3w&s",
+            para: `The Tower of London is widely believed to be one of the most haunted places in London, with numerous ghost stories and sightings associated with it. Many of the reported paranormal experiences involve historical figures who were imprisoned and died within its walls, most notably Anne Boleyn. `
         },
         {
-            name: "(iv) St. Peter’s Seminary",
-            link: "https://upload.wikimedia.org/wikipedia/commons/c/c2/GK%26C_St_Peter%27s_chapel.jpg",
-            para: "Located in Scotland, this Brutalist Catholic seminary is a protected structure, famous for its architecture and decay."
+            name: "The Ancient Ram Inn",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3rl6cap9AW72IFCxvEJtIyLYjaLGNqLJx8Q&s",
+            para: `The Ancient Ram Inn, located in Wotton-under-Edge, Gloucestershire, is a 1,000-year-old building with a reputation for being one of the most haunted locations in England. It's a Grade II* listed building and a former pub, but is now primarily a paranormal event location and tourist attraction. `
         },
         {
-            name: "(v) The Maunsell Sea Forts",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTEKEIuRoLuqUUCeeryQGXp7n5rvBkzyhkIZuzInFsFBVkerg&s",
-            para: "Built during WWII in the Thames/Mersey estuaries, these forts defended the UK from enemy aircraft and ships."
-        }
+            name: "Pendle Hill",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ477f4FGdYDMaJRV2sd4w3nM6hRAT2eIi5w&s",
+            para: `Pendle Hill in Lancashire is renowned for its historical association with the Pendle witch trials of 1612 and is considered a haunted location, with reports of paranormal activity linked to the trials. The area, including nearby villages and farms, is said to be where the accused witches lived and where rituals were performed. `
+        },
+        {
+            name: "Pluckley ",
+            link: "https://images.timesnownews.com/thumb/msid-151496269,thumbsize-72620,width-400,height-225,resizemode-75/151496269.jpg",
+            para: `Pluckley, a village in Kent, England, is widely known as "the most haunted village in England". It gained this reputation due to numerous ghost stories and paranormal accounts associated with the village. These stories have been documented and popularized in books and local folklore for over 70 years`
+        },
+
     ];
 
     const fadeInUp = {
@@ -54,13 +60,13 @@ export default function United_Kingdom2() {
                 className="py-10"
                 style={{
                     backgroundImage:
-                        'url(https://media.istockphoto.com/id/901657092/vector/watercolor-painted-flag.jpg?s=612x612&w=0&k=20&c=2q4z0MbRJ5y_8ScECcwD0ckO8sgh6-pYvEa9JL0N6U4=)',
+                        'url(https://c4.wallpaperflare.com/wallpaper/950/516/946/flag-britain-color-texture-wallpaper-thumb.jpg)',
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                 }}
             >
-                <h1 className="text-6xl font-extrabold text-center text-black drop-shadow-md">3) UNITED KINGDOM</h1>
+                <h1 className="text-6xl font-extrabold text-center text-white drop-shadow-md">3) UNITED KINGDOM</h1>
 
                 <div className="py-20 space-y-20 px-6 md:px-10">
                     {united.map((item, key) => (
@@ -72,17 +78,17 @@ export default function United_Kingdom2() {
                             viewport={{ once: false, amount: 0.5 }}
                             className={`md:flex items-center gap-10 py-10 ${key % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}
                         >
-                            <div className="text-2xl md:text-3xl font-bold text-black text-center md:text-left w-full md:w-1/3">
+                            <div className="text-2xl md:text-3xl font-bold text-white text-center md:text-left w-full md:w-1/3">
                                 {item.name}
                             </div>
 
-                            <div className="flex flex-col items-center text-center max-w-md mx-auto bg-yellow-300 bg-opacity-90 rounded-xl shadow-xl p-6 transition duration-500 hover:scale-105 hover:shadow-2xl w-full md:w-2/3">
+                            <div className="flex flex-col items-center text-center max-w-md mx-auto bg-red-500 bg-opacity-90 rounded-xl shadow-xl p-6 transition duration-500 hover:scale-105 hover:shadow-2xl w-full md:w-2/3">
                                 <img
                                     className="h-60 w-80 object-cover rounded-md mb-4 transition-transform duration-300 hover:rotate-1 hover:scale-105"
                                     src={item.link}
                                     alt={item.name}
                                 />
-                                <p className="text-lg font-medium text-gray-700">
+                                <p className="text-lg font-medium text-cyan-300">
                                     {item.para}
                                 </p>
                             </div>
--- vite-project/src/assets/Components2/United_States2.jsx
+++ vite-project/src/assets/Components2/United_States2.jsx
@@ -7,30 +7,40 @@ import Home2 from './Home2';
 export default function United_States2() {
     const united = [
         {
-            name: "(i) Centralia",
-            link: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Graffiti_Highway_-_Centralia%2C_Pennsylvania_%282019%29_b.jpg/250px-Graffiti_Highway_-_Centralia%2C_Pennsylvania_%282019%29_b.jpg",
-            para: `Centralia, Pennsylvania, is a near-ghost town known for its persistent underground coal mine fire that has been burning since 1962.`
+            name: " Willows Weep",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjepKQkUdzwiXAvsnQtMWfhEhpfwUKbIJP7nBdXQ4FpjwwYxE7LV4NrnynKYLtkB0S6Fg&usqp=CAU",
+            para : `Willows Weep, located in Cayuga, Indiana, is a house notorious for its dark reputation as one of the most haunted and potentially evil places in America`
         },
-        {
-            name: "(ii) Hashima Island",
-            link: "https://i.insider.com/59a9b31952c08026008b4a92?width=800&format=jpeg&auto=webp",
-            para: "Hashima Island, aka Battleship Island, is in Japan, but often compared to North Brother Island, NY, for its haunting abandoned infrastructure."
+         {
+            name: "Lake Shawnee  ",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSl3QWdPFot56oBKSt_MXVGxNz2Hnnl_yDPw&s",
+            para : `Lake Shawnee Abandoned Amusement Park, located in Princeton, West Virginia, is known for its tragic history and eerie atmosphere. It was built on a site with a dark past, including a Native American burial ground and a brutal massacre of settlers`
         },
-        {
-            name: "(iii) City Hall Subway Station",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1LT9828xOsLCR4BTIgatvdQ1Zvrr-5NuJPLS8MDNy1eZos00g&s",
-            para: "A stunning former subway terminal in NYC, now closed to the public but used for turning 6 trains around in a historic loop."
+         {
+            name: " Skinwalker Ranch",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhgFV83hgAETA_SM65IblD8ruiiZFkQOhqfA&s",
+            para : `Skinwalker Ranch, also known as Sherman Ranch, is a property in Utah rumored to be the site of paranormal and UFO-related phenomena. The ranch's name is derived from the Navajo legend of the skin-walker, a malevolent shapeshifting entity`
         },
-        {
-            name: "(iv) Eastern State Penitentiary",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoH4kFpraDnk1Og1yWt5rKYGaC3BlhTfeCrQ&s",
-            para: "An imposing Philadelphia prison, it was once the most expensive prison in the world and introduced solitary confinement as a concept."
+         {
+            name: "Bell Witch Cave ",
+            link: "https://www.bellwitchcave.com/wp-content/themes/yootheme/cache/IMG_8014-1-scaled-5611ca3d.jpeg",
+            para : `The Bell Witch Cave, located in Adams, Tennessee, is a key site in the Bell Witch legend, a famous American haunting story.Beginning in 1817 and continuing until 1821, John Bell and his family were allegedly “haunted” by a devilish spirit called a “witch” known as “Kate.”`
         },
-        {
-            name: "(v) Detroit’s Packard Plant",
-            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs5-sNa2_cw6I06Ew5lrap_S_ikb3fLCooxQ&s",
-            para: "Detroit’s Packard Plant was a state-of-the-art facility for luxury cars in the early 1900s, now standing abandoned as a symbol of decline."
-        }
+         {
+            name: "LaLaurie Mansion ",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVGvlIYjDeMYF_cxknQ6YtXSJLmNENYWFI5Q&s",
+            para : `The LaLaurie Mansion, located at 1140 Royal Street in the French Quarter of New Orleans, Louisiana, is one of the most infamous haunted houses in the United States.In 1834, a fire broke out in the mansion, leading to the horrifying discovery of enslaved individuals who had been brutally tortured and kept in deplorable conditions in a locked attic room `
+        },
+         {
+            name: "Bachelor’s Grove Cemetery ",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEz52kb-wtCYM0_yDy9lggZBzNC6ZI7UJhsg&s",
+            para : `Bachelor’s Grove Cemetery, located near 143rd Street and Ridgeland Avenue in Midlothian, Illinois, is a small, abandoned burial ground it originally served as a final resting place for early settlers and German immigrants. Though it was once a peaceful rural cemetery, by the 1960s it fell into neglect and became a hotspot for vandalism and illegal activity`
+        },
+         {
+            name: "Washington Square Park ",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMFVBOQbXrJA96fl8_1liKwyaJnfBg6qR6hg&s",
+            para: `Washington Square Park is located in the Greenwich Village neighborhood of Lower Manhattan, New York City. Nearby New York University's Washington Square Campus, particularly the Elmer Holmes Bobst Library, has witnessed multiple suicides, leading to reports of strange sounds, including the rustling of papers, and the appearance of a ghostly librarian.`
+        },
     ];
 
     const fadeInUp = {
@@ -52,13 +62,13 @@ export default function United_States2() {
             <div
                 className="py-10"
                 style={{
-                    backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsW9CtB-P_GT--m2MhGfqoXZ2ZU3appJUHKw&s)',
+                    backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_bhA7mH1_ZvSYTgbNTasO-a5l1ochh0ilIQ&s)',
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                 }}
             >
-                <h1 className="text-6xl font-extrabold text-center text-black drop-shadow-md">2) UNITED STATES</h1>
+                <h1 className="text-6xl font-extrabold text-center text-white drop-shadow-md">2) UNITED STATES</h1>
 
                 <div className="py-20 space-y-20 px-6 md:px-10">
                     {united.map((item, key) => (
@@ -70,17 +80,17 @@ export default function United_States2() {
                             viewport={{ once: false, amount: 0.5 }}
                             className={`md:flex items-center gap-10 py-10 ${key % 2 === 0 ? 'flex-row' : 'flex-row-reverse'}`}
                         >
-                            <div className="text-2xl md:text-3xl font-bold text-black text-center md:text-left w-full md:w-1/3">
+                            <div className="text-2xl md:text-3xl font-bold text-white text-center md:text-left w-full md:w-1/3">
                                 {item.name}
                             </div>
 
-                            <div className="flex flex-col items-center text-center max-w-md mx-auto bg-yellow-300 bg-opacity-90 rounded-xl shadow-xl p-6 transition duration-500 hover:scale-105 hover:shadow-2xl w-full md:w-2/3">
+                            <div className="flex flex-col items-center text-center max-w-md mx-auto bg-red-500 bg-opacity-90 rounded-xl shadow-xl p-6 transition duration-500 hover:scale-105 hover:shadow-2xl w-full md:w-2/3">
                                 <img
                                     className="h-60 w-80 object-cover rounded-md mb-4 transition-transform duration-300 hover:rotate-1 hover:scale-105"
                                     src={item.link}
                                     alt={item.name}
                                 />
-                                <p className="text-lg font-medium text-gray-700">
+                                <p className="text-lg font-medium text-cyan-300">
                                     {item.para}
                                 </p>
                             </div>
